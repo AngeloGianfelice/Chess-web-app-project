3aa8e29c18993ecf2f087eb2af911b52d28c46f4
AngeloGianfelice <angelo_gianfelice@yahoo.com>
2021-05-15 23:22:19 +0200

    news page improvement
    -added top 10 live rating table;
    -added ausiliaries function to save ~100 lines of code;
    -improved news page layout;

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const saltRounds = 12;
 
 //database packages
 const mongoose = require("mongoose");
+const { allowedNodeEnvironmentFlags } = require('process');
 mongoose.connect("mongodb+srv://lewis:cjb07@cluster0.ysajr.mongodb.net/databaseUtenti", { useUnifiedTopology: true, useNewUrlParser: true });
 const schemaUtente = {
     username: String,
@@ -178,29 +179,49 @@ app.post("/updateStats", async (req, res) => {
 });
 
 app.post('/fide', async (req, res) => {
+    //route news scraping
     const scraped = await scrapesite("https://www.fide.com/news").catch(err => {
         console.log(err);
-    });
-        
+    });  
     res.json(scraped);
 });
- 
 
+app.post('/ratings', async (req,res) =>{
+    //route ratings scraping
+    const data= await scraperatings("https://ratings.fide.com").catch(err => {
+        console.log(err);
+    });
+    res.json(data);
+});
+ 
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Errore nell'inizializzazione del server");
+    } else {
+        console.log("listening on port: ", port);
+    }
+});
+
+//funzioni ausiliarie
 async function scrapesite(url) {
-    const browser = await puppeteer.launch({args:['--no-sandbox']});
+    //funzione di webscraping
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto(url);
+    //scraping immagini
     const [e1] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[2]/app-client-news-general[1]/a/img");
     const [e2] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[2]/app-client-news-general[2]/a/img");
     const [e3] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[3]/app-client-news-blog[1]/a/div[1]/img");
     const [e4] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[3]/app-client-news-blog[2]/a/div[1]/img");
     const [e5] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[3]/app-client-news-blog[3]/a/div[1]/img");
+    //scraping links
     const [l1] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[2]/app-client-news-general[1]/a");
     const [l2] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[2]/app-client-news-general[2]/a");
     const [l3] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[3]/app-client-news-blog[1]/a");
     const [l4] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[3]/app-client-news-blog[2]/a");
     const [l5] = await page.$x("/html/body/app-root/app-client/app-news/app-client-news-top/section/div[1]/div/div[3]/app-client-news-blog[3]/a");
 
+    //scraping ratings ...
     const param1 = await getproperties(e1, l1);
     const param2 = await getproperties(e2, l2);
     const param3 = await getproperties(e3, l3);
@@ -225,6 +246,7 @@ async function scrapesite(url) {
     browser.close();
     return { srcTxt1, titleTxt1, srcTxt2, titleTxt2, srcTxt3, titleTxt3, srcTxt4, titleTxt4, srcTxt5, titleTxt5, link1, link2, link3, link4, link5 };
 }
+
 async function getproperties(Xpath, Lpath) {
     const src = await Xpath.getProperty("src");
     const title = await Xpath.getProperty("title");
@@ -236,10 +258,80 @@ async function getproperties(Xpath, Lpath) {
 
 }
 
-app.listen(port, (error) => {
-    if (error) {
-        console.log("Errore nell'inizializzazione del server");
-    } else {
-        console.log("listening on port: ", port);
-    }
-})
+async function scraperatings(url){
+    //funzione di webscraping
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    //scraping nomi
+    const nome1=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[1]/td[2]/a',page);
+    const nome2=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[2]/td[2]/a',page);
+    const nome3=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[3]/td[2]/a',page);
+    const nome4=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[4]/td[2]/a',page);
+    const nome5=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[5]/td[2]/a',page);
+    const nome6=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[6]/td[2]/a',page);
+    const nome7=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[7]/td[2]/a',page);
+    const nome8=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[8]/td[2]/a',page);
+    const nome9=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[9]/td[2]/a',page);
+    const nome10=await fetch_nomi('//*[@id="top_rating_div"]/table/tbody/tr[10]/td[2]/a',page);
+
+    //scraping federazione
+    const fed1=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[1]/td[3]',page);
+    const fed2=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[2]/td[3]',page);
+    const fed3=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[3]/td[3]',page);
+    const fed4=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[4]/td[3]',page);
+    const fed5=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[5]/td[3]',page);
+    const fed6=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[6]/td[3]',page);
+    const fed7=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[7]/td[3]',page);
+    const fed8=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[8]/td[3]',page);
+    const fed9=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[9]/td[3]',page);
+    const fed10=await fetch_federazione('//*[@id="top_rating_div"]/table/tbody/tr[10]/td[3]',page);
+
+    //scraping rating
+    const rating1=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[1]/td[4]',page);
+    const rating2=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[2]/td[4]',page);
+    const rating3=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[3]/td[4]',page);
+    const rating4=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[4]/td[4]',page);
+    const rating5=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[5]/td[4]',page);
+    const rating6=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[6]/td[4]',page);
+    const rating7=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[7]/td[4]',page);
+    const rating8=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[8]/td[4]',page);
+    const rating9=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[9]/td[4]',page);
+    const rating10=await fetch_ratings('//*[@id="top_rating_div"]/table/tbody/tr[10]/td[4]',page);
+
+    browser.close();
+    return{
+            primo:[nome1,fed1.img_src,fed1.img_text,rating1],
+            secondo:[nome2,fed2.img_src,fed2.img_text,rating2],
+            terzo:[nome3,fed3.img_src,fed3.img_text,rating3],
+            quarto:[nome4,fed4.img_src,fed4.img_text,rating4],
+            quinto:[nome5,fed5.img_src,fed5.img_text,rating5],
+            sesto:[nome6,fed6.img_src,fed6.img_text,rating6],
+            settimo:[nome7,fed7.img_src,fed7.img_text,rating7],
+            ottavo:[nome8,fed8.img_src,fed8.img_text,rating8],
+            nono:[nome9,fed9.img_src,fed9.img_text,rating9],
+            decimo:[nome10,fed10.img_src,fed10.img_text,rating10],
+        };
+}
+async function fetch_nomi(Xpath,page){
+    const [name]=await page.$x(Xpath);
+    const nametext=await name.getProperty("textContent");
+    const nome=await nametext.jsonValue();
+    return nome;
+}
+async function fetch_ratings(Xpath,page){
+    const [ratingpath]=await page.$x(Xpath);
+    const ratingtext=await ratingpath.getProperty("textContent");
+    const rating=await ratingtext.jsonValue();
+    return rating;
+}
+async function fetch_federazione(Xpath,page){
+    const [fed]=await page.$x(Xpath+'/img');
+    const [fedtext]=await page.$x(Xpath);
+    const fed_img_src=await fed.getProperty("src");
+    const fed_img_text=await fedtext.getProperty("textContent");
+    const img_src=await fed_img_src.jsonValue();
+    const img_text=await fed_img_text.jsonValue();
+    return {img_src, img_text};
+}
--- public/js/webscraper.js
+++ public/js/webscraper.js
@@ -3,9 +3,11 @@ fetchnews().catch(err => {
 });
 
 async function fetchnews() {
+    //fetch news 
     const response = await fetch("/fide", { method: "POST", body: "application/json"});
     const file = await response.json();
 
+    //aggiorno titoli e foto
     $('#foto1').attr('src', file.srcTxt1);
     $('#titolo1').text(file.titleTxt1);
     $('#foto2').attr('src', file.srcTxt2);
@@ -16,13 +18,69 @@ async function fetchnews() {
     $('#titolo4').text(file.titleTxt4);
     $('#foto5').attr('src', file.srcTxt5);
     $('#titolo5').text(file.titleTxt5);
-    //titolo
+    //aggiorno link
     $('#link1').attr('href', file.link1);
     $('#link2').attr('href', file.link2);
     $('#link3').attr('href', file.link3);
     $('#link4').attr('href', file.link4);
     $('#link5').attr('href', file.link5);
     $('.loader').css("display", "none");
+
+    //fetch ratings 
+    const rating_table= await fetch('/ratings', { method: "POST", body: "application/json"});
+    const file_table = await rating_table.json();
+    //#1 riga
+    $('.table td').eq(1).html(file_table.primo[0]);
+    $('#img1').attr('src',file_table.primo[1])
+    $('#text1').text(file_table.primo[2]);
+    $('.table td').eq(3).html(file_table.primo[3]);
+    //#2 riga
+    $('.table td').eq(5).html(file_table.secondo[0]);
+    $('#img2').attr('src',file_table.secondo[1])
+    $('#text2').text(file_table.secondo[2]);
+    $('.table td').eq(7).html(file_table.secondo[3]);
+    //#3 riga
+    $('.table td').eq(9).html(file_table.terzo[0]);
+    $('#img3').attr('src',file_table.terzo[1])
+    $('#text3').text(file_table.terzo[2]);
+    $('.table td').eq(11).html(file_table.terzo[3]);
+    //#4 riga
+    $('.table td').eq(13).html(file_table.quarto[0]);
+    $('#img4').attr('src',file_table.quarto[1])
+    $('#text4').text(file_table.quarto[2]);
+    $('.table td').eq(15).html(file_table.quarto[3]);
+    //#5 riga 
+    $('.table td').eq(17).html(file_table.quinto[0]);
+    $('#img5').attr('src',file_table.quinto[1])
+    $('#text5').text(file_table.quinto[2]);
+    $('.table td').eq(19).html(file_table.quinto[3]);
+    //#6 riga
+    $('.table td').eq(21).html(file_table.sesto[0]);
+    $('#img6').attr('src',file_table.sesto[1])
+    $('#text6').text(file_table.sesto[2]);
+    $('.table td').eq(23).html(file_table.sesto[3]);
+    //#7 riga
+    $('.table td').eq(25).html(file_table.settimo[0]);
+    $('#img7').attr('src',file_table.settimo[1])
+    $('#text7').text(file_table.settimo[2]);
+    $('.table td').eq(27).html(file_table.settimo[3]);
+    //#8 riga
+    $('.table td').eq(29).html(file_table.ottavo[0]);
+    $('#img8').attr('src',file_table.ottavo[1])
+    $('#text8').text(file_table.ottavo[2]);
+    $('.table td').eq(31).html(file_table.ottavo[3]);
+    //#9 riga 
+    $('.table td').eq(33).html(file_table.nono[0]);
+    $('#img9').attr('src',file_table.nono[1])
+    $('#text9').text(file_table.nono[2]);
+    $('.table td').eq(35).html(file_table.nono[3]);
+    //#10 riga
+    $('.table td').eq(37).html(file_table.decimo[0]);
+    $('#img10').attr('src',file_table.decimo[1])
+    $('#text10').text(file_table.decimo[2]);
+    $('.table td').eq(39).html(file_table.decimo[3]);
+    $('.mini').css("display", "none");
+
 }
 
 
